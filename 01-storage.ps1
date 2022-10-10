e9093d4b1f7bf8ae2d100d865d940464541bdb3a
Venkatarajesh Jakka <venkatarajeshjakka@Venkatarajeshs-MacBook-Air.local>
2022-10-10 15:10:46 +0530

    first commit

---
+++ 01-storage.ps1
@@ -0,0 +1,11 @@
+$rg = 'arm-template-01'
+
+New-AzResourceGroup -Name $rg -Location centralindia -Force
+
+New-AzResourceGroupDeployment `
+    -Name 'new-storage' `
+    -ResourceGroupName $rg `
+    -TemplateFile '01-storage.json'`
+    -storageName 'amdemostorage02a' `
+    -storageSKU 'Standard_LRS' `
+    -storageTier 'Standard'
